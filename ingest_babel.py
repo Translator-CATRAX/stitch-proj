f6bfd525cc0d091e8ecdaaa0ebc0b1c7be769731
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2025-02-26 11:26:28 -0800

    updated

--- ingest_babel.py
+++ ingest_babel.py
@@ -13,6 +13,16 @@
 # in about 24 hours, assuming that all SELECTs are using indexes
 # as was intended (use of indexes needs to be empirically verified).
 
+# # How to run the Babel ingest
+# - `ssh ubuntu@stitch.rtx.ai`
+# - `cd stitch`
+# - `screen`
+# - `source venv/bin/activate`
+# - `python3.12 -u ingest_babel.py > ingest_babel.log 2>&1`
+# - `ctrl-X D` (to exit the screen session)
+# - `tail -f ingest_babel.log` (so you can watch progress)
+# - In another terminal session, watch memory usage using `top`
+
 import bmt
 import htmllistparse
 import math
