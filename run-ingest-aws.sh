a0704dc205e032dec922fac7729266f5d748a4b0
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2025-03-27 13:13:08 -0700

    #12

--- run-ingest-aws.sh
+++ run-ingest-aws.sh
@@ -7,7 +7,8 @@ STITCH_TMP_DIR=/mnt/data/stitch/tmp
 STITCH_LOG_FILE=${STITCH_DIR}/ingest-babel.log
 STITCH_SQLITE_FILE=${STITCH_DIR}/babel.sqlite
 BABEL_COMPENDIA_BASE_URL=https://stars.renci.org/var/babel_outputs/2025jan23/compendia/
-           
+
+export SQLITE_TMPDIR=${STITCH_TMP_DIR}
 rm -r -f ${STITCH_TMP_DIR}
 mkdir -p ${STITCH_TMP_DIR}
 
