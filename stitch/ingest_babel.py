348f6e858630fbcda0bf0190124921aecbce9f1e
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2025-07-22 21:47:22 -0700

    #17

--- stitch/ingest_babel.py
+++ stitch/ingest_babel.py
@@ -858,10 +858,7 @@ def _customize_temp_dir(temp_dir: str,
     os.environ["SQLITE_TMPDIR"] = temp_dir
     if not no_exec:
         python_exe = sys.executable
-        exe_args = [python_exe]
-        if sys.flags.unbuffered:
-            exe_args.append("-u")
-        new_args = exe_args + [sys.argv[0], *sys.argv[1:], "--no-exec"]
+        new_args = [python_exe, "-u", sys.argv[0], *sys.argv[1:], "--no-exec"]
         os.execve(python_exe, new_args, os.environ.copy())
     os.environ["RAY_TMPDIR"] = temp_dir
     tempfile.tempdir = temp_dir
