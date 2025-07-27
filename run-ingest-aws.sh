ed31a695854bd433f54e72f85ee82af2f330951f
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2025-07-26 19:38:15 -0700

    #21 #17

--- run-ingest-aws.sh
+++ run-ingest-aws.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
-
 set -o nounset -o pipefail -o errexit
 
 STITCH_DIR=/home/ubuntu/stitch
-STITCH_TMP_DIR=/home/ubuntu/stitch/tmp
+# This next line is commented out because I think we have successfully
+# resolved issue 17, with the temp dir being able to be specified only
+# via the "--temp-dir" command-line option for ingest_babel.py:
+# STITCH_TMP_DIR=/home/ubuntu/stitch/tmp
 STITCH_LOG_FILE=${STITCH_DIR}/ingest-babel.log
 STITCH_SQLITE_FILE=${STITCH_DIR}/babel.sqlite
-BABEL_COMPENDIA_BASE_URL=https://stars.renci.org/var/babel_outputs/2025mar31/compendia/
-BABEL_CONFLATION_BASE_URL=https://stars.renci.org/var/babel_outputs/2025mar31/conflation/
+BABEL_BASE_URL=https://stars.renci.org/var/babel_outputs/2025mar31
+BABEL_COMPENDIA_BASE_URL=${BABEL_BASE_URL}/compendia/
+BABEL_CONFLATION_BASE_URL=${BABEL_BASE_URL}/conflation/
 
 export SQLITE_TMPDIR=${STITCH_TMP_DIR}
 rm -r -f ${STITCH_TMP_DIR}
