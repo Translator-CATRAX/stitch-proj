3a4b31d9c845ac25595dfe23dd5716ff48032822
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2025-07-31 20:26:56 -0700

    #31

---
+++ run-integration-tests.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -o nounset -o pipefail -o errexit
+
+STITCH_DIR=.
+# This next line is commented out because I think we have successfully
+# resolved issue 17, with the temp dir being able to be specified only
+# via the "--temp-dir" command-line option for ingest_babel.py:
+STITCH_LOG_FILE=${STITCH_DIR}/run-integration-tests.log
+STITCH_SQLITE_FILE=${STITCH_DIR}/babel.sqlite
+BABEL_BASE_URL=https://stars.renci.org/var/babel_outputs/2025mar31
+BABEL_COMPENDIA_BASE_URL=${BABEL_BASE_URL}/compendia/
+BABEL_CONFLATION_BASE_URL=${BABEL_BASE_URL}/conflation/
+
+rm -f ${STITCH_LOG_FILE}
+
+${STITCH_DIR}/venv/bin/python3.12 -u ${STITCH_DIR}/stitch/ingest_babel.py \
+             --babel-compendia-url ${BABEL_COMPENDIA_BASE_URL} \
+             --babel-conflation-url ${BABEL_CONFLATION_BASE_URL} \
+             --database-file-name ${STITCH_SQLITE_FILE} \
+             --test-type=1
+             >>${STITCH_LOG_FILE} 2>&1
+
+${STITCH_DIR}/venv/bin/python3.12 -u ${STITCH_DIR}/stitch/ingest_babel.py \
+             --babel-compendia-url ${BABEL_COMPENDIA_BASE_URL} \
+             --babel-conflation-url ${BABEL_CONFLATION_BASE_URL} \
+             --database-file-name ${STITCH_SQLITE_FILE} \
+             --test-type=2
+             >>${STITCH_LOG_FILE} 2>&1
+
+${STITCH_DIR}/venv/bin/python3.12 -u ${STITCH_DIR}/stitch/ingest_babel.py \
+             --babel-compendia-url ${BABEL_COMPENDIA_BASE_URL} \
+             --babel-conflation-url ${BABEL_CONFLATION_BASE_URL} \
+             --database-file-name ${STITCH_SQLITE_FILE} \
+             --test-type=3
+             >>${STITCH_LOG_FILE} 2>&1
