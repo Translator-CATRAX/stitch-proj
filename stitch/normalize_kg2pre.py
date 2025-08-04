43ed3a5a88db9bd6094fc45205844fa4fb823969
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2025-08-04 16:03:14 -0700

    #40

--- stitch/normalize_kg2pre.py
+++ stitch/normalize_kg2pre.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3.12
 
 import argparse
-import bmt
+import functools
 import itertools as it
-import json
-import pprint
-import pandas as pd
+import math
+import multiprocessing
+import operator
 import sqlite3
+from collections.abc import Callable, Iterable
+from typing import Any, Iterator, Optional, TypeVar
+
+import bmt
+import pandas as pd
+import tqdm
+
 import local_babel as lb
 import stitchutils as su
-from typing import Any, Callable, Iterable, Optional
 
+T = TypeVar("T")
+R = TypeVar("R")
+
+DEFAULT_CHUNK_SIZE = 10_000
+DEFAULT_ESTIM_NUM_EDGES = 27_257_461
 
 def _predicate_curie_to_space_case(curie: str) -> str: # noqa
     return curie[len('biolink:'):].replace('_', ' ')
@@ -34,13 +45,17 @@ def _get_args() -> argparse.Namespace:
                     type=str,
                     help=('the edges JSON lines file to which the output should be '
                           'saved'))
+    ap.add_argument('--chunk_size',
+                    default=DEFAULT_CHUNK_SIZE,
+                    type=int,
+                    dest='chunk_size',
+                    help=('number of jsonlines lines to be read into a single chunk'))
+    ap.add_argument('--estim_num_edges',
+                    default=DEFAULT_ESTIM_NUM_EDGES,
+                    type=int,
+                    dest='estim_num_edges',
+                    help=('number of jsonlines lines in the input edges file'))
     return ap.parse_args()
-
-def _read_jsonl_chunks(filename: str,
-                       lines_per_chunk: int) -> Iterable[pd.DataFrame]:
-    return pd.read_json(filename,
-                        lines=True,
-                        chunksize=lines_per_chunk)
 
 EDGE_PROPERTIES_COPY_FROM_KG2PRE = \
     ('agent_type',
@@ -54,13 +69,15 @@ EDGE_PROPERTIES_COPY_FROM_KG2PRE_IF_EXIST = \
      'qualified_object_direction',
      'qualified_object_aspect')
 
-PREDICATE_CURIES_SKIP = tuple()
-#    ('biolink:same_as',
+PREDICATE_CURIES_SKIP: tuple[str, ...] = \
+    (
+        'biolink:same_as',
 #     'biolink:related_to',
 #     'biolink:close_match',
 #     'biolink:subclass_of',
 #     'biolink:has_subclass',
 #     'biolink:exact_match')
+    )
 
 def _pick_category(categories: set[str],
                        sub_obj: str,
@@ -118,91 +135,110 @@ def _fix_curie_if_broken(curie: str) -> str:
         curie = 'NCIT:' + curie[len('OBO:NCIT_'):]
     return curie
 
-def _make_process_edges_row(conn: sqlite3.Connection) -> Callable:
-    def process_edges_row(edge_series: pd.Series) -> \
-            tuple[tuple[Optional[dict[str, Any]], str, str], ...]:
-        edge = edge_series.to_dict()
-        kg2pre_edge_id = edge['id']
-        res_edge = {k: edge[k] for k in EDGE_PROPERTIES_COPY_FROM_KG2PRE}
-        res_edge['id'] = None  # this will eventually be a global integer index
-                               # of the edge in a list of all edges (can't compute
-                               # that information here since we are processing one
-                               # edge only, within this function
-        res_edge['kg2_ids'] = [kg2pre_edge_id]
-        predicate = res_edge['predicate']
-        if predicate in PREDICATE_CURIES_SKIP:
-            return ((None,
-                     kg2pre_edge_id,
-                     f"predicate is on the skip list: {predicate}"),)
-        res_edge.update({k: edge[k] for k in EDGE_PROPERTIES_COPY_FROM_KG2PRE_IF_EXIST
-                         if su.nan_to_none(edge[k])})
-        kg2pre_subject_curie = _fix_curie_if_broken(edge['subject'])
-        pref_curie_tuple = lb.map_curie_to_preferred_curies(conn,
-                                                            kg2pre_subject_curie)
-        picked_pref_curies_subject = _filter_pref_curies(pref_curie_tuple,
-                                                         "subject",
-                                                         predicate)
-        if len(picked_pref_curies_subject)==0:
-            return ((None, kg2pre_edge_id,
-                     "unable to find preferred CURIE for subject: "
-                     f"{kg2pre_subject_curie}"),)
-        kg2pre_object_curie = _fix_curie_if_broken(edge['object'])
-        pref_curie_tuple = lb.map_curie_to_preferred_curies(conn,
-                                                            kg2pre_object_curie)
-        picked_pref_curies_object = _filter_pref_curies(pref_curie_tuple,
-                                                        "object",
-                                                        predicate)
-        if len(picked_pref_curies_object)==0:
-            return ((None, kg2pre_edge_id,
-                     "unable to find preferred CURIE for object: "
-                     f"{kg2pre_object_curie}"),)
-        if len(picked_pref_curies_subject) > 2 or len(picked_pref_curies_object) > 2:
-            print(edge_series)
-            assert False
-        res: list[tuple[Optional[dict[str, Any]], str, str]] = []
-        for subject_curie, object_curie in it.product(picked_pref_curies_subject,
-                                                      picked_pref_curies_object):
-            new_res_edge = res_edge
-            new_res_edge['subject'] = subject_curie
-            new_res_edge['object'] = object_curie
-            res.append((new_res_edge, kg2pre_edge_id, 'OK'))
-        if len(res) == 0:
-            res.append((None,
-                        kg2pre_edge_id,
-                        "no preferred curies available"))
-        return tuple(res)
-    return process_edges_row
-
-def _process_edges_row(row: pd.Series) -> \
+def _process_edges_row(conn: sqlite3.Connection,
+                       edge_series: pd.Series) -> \
         tuple[tuple[Optional[dict[str, Any]], str, str], ...]:
-    return tuple()
+    edge = edge_series.to_dict()
+    kg2pre_edge_id = edge['id']
+    res_edge = {k: edge[k] for k in EDGE_PROPERTIES_COPY_FROM_KG2PRE}
+    res_edge['id'] = None  # this will eventually be a global integer index
+                           # of the edge in a list of all edges (can't compute
+                           # that information here since we are processing one
+                           # edge only, within this function
+    res_edge['kg2_ids'] = [kg2pre_edge_id]
+    predicate = res_edge['predicate']
+    if predicate in PREDICATE_CURIES_SKIP:
+        return ((None,
+                 kg2pre_edge_id,
+                 f"predicate is on the skip list: {predicate}"),)
+    res_edge.update({k: edge[k] for k in \
+                     EDGE_PROPERTIES_COPY_FROM_KG2PRE_IF_EXIST
+                     if su.nan_to_none(edge[k])})
+    kg2pre_subject_curie = _fix_curie_if_broken(edge['subject'])
+    pref_curie_tuple = lb.map_curie_to_preferred_curies(conn,
+                                                        kg2pre_subject_curie)
+    picked_pref_curies_subject = _filter_pref_curies(pref_curie_tuple,
+                                                     "subject",
+                                                     predicate)
+    if len(picked_pref_curies_subject)==0:
+        return ((None, kg2pre_edge_id,
+                 "unable to find preferred CURIE for subject: "
+                 f"{kg2pre_subject_curie}"),)
+    kg2pre_object_curie = _fix_curie_if_broken(edge['object'])
+    pref_curie_tuple = lb.map_curie_to_preferred_curies(conn,
+                                                        kg2pre_object_curie)
+    picked_pref_curies_object = _filter_pref_curies(pref_curie_tuple,
+                                                    "object",
+                                                    predicate)
+    if len(picked_pref_curies_object)==0:
+        return ((None, kg2pre_edge_id,
+                 "unable to find preferred CURIE for object: "
+                 f"{kg2pre_object_curie}"),)
+    if len(picked_pref_curies_subject) > 2 or \
+       len(picked_pref_curies_object) > 2:
+        print(edge_series)
+        assert False
+    res: list[tuple[Optional[dict[str, Any]], str, str]] = []
+    for subject_curie, object_curie in it.product(picked_pref_curies_subject,
+                                                  picked_pref_curies_object):
+        new_res_edge = res_edge
+        new_res_edge['subject'] = subject_curie
+        new_res_edge['object'] = object_curie
+        res.append((new_res_edge, kg2pre_edge_id, 'OK'))
+    if len(res) == 0:
+        res.append((None,
+                    kg2pre_edge_id,
+                    "no preferred curies available"))
+    return tuple(res)
 
-def _process_chunk_of_edges(edge_chunk: pd.DataFrame) -> \
-        Iterable[tuple[Optional[dict[str, Any]], str, str]]:
-    t = it.chain.from_iterable(tuple(_process_edges_row(row)) \
-                               for _, row in edge_chunk.iterrows())
-    return filter(lambda st: st[0] is not None, t)
+
+def _make_process_chunk_of_edges(db_filename: str) -> Callable:
+    def process_chunk_of_edges(edge_chunk: pd.DataFrame) -> \
+            Iterable[tuple[Optional[dict[str, Any]], str, str]]:
+        with lb.connect_to_db_read_only(db_filename) as conn:
+            process_edges = functools.partial(_process_edges_row, conn)
+            # iterate over rows in the data frame
+            row_iter = edge_chunk.iterrows()
+
+            # each row contains a tuple of a row index and the dictionary contents
+            # of the edge; just grab the latter
+            edges_iter = map(operator.itemgetter(1), row_iter)
+
+            # process the edges in this single chunk
+            mapped_edges_iter = map(process_edges, edges_iter)
+
+            # handle cases where a single KG2pre edge got mapped to multiple KG2c edges
+            chained_iter = it.chain.from_iterable(mapped_edges_iter)
+
+            # filter out the edges that failed to map
+            filter_iter = filter(lambda st: st[0] is not None, chained_iter)
+        return filter_iter
+    return process_chunk_of_edges
+
 
 def main(nodes_file: str,
          edges_file: str,
          babel_db: str,
-         edges_output_file: str):
+         edges_output_file: str,
+         chunk_size: int,
+         estim_num_edges: int):
     print(f"nodes file is: {nodes_file}")
     print(f"edges file is: {edges_file}")
+    print(f"babel-db file is: {babel_db}")
+    estim_num_chunks = math.ceil(estim_num_edges / chunk_size)
+    print(f"number of chunks: {estim_num_chunks}")
     global _pick_category
     _pick_category = _make_pick_category()
-    with lb.connect_to_db_read_only(babel_db) as conn:
-        global _process_edges_row
-        _process_edges_row = _make_process_edges_row(conn)
-        chunks_iter = _read_jsonl_chunks(edges_file, 100_000)
-        edge_tuples = tuple(t[0] for t in
-                            it.chain.from_iterable(map(_process_chunk_of_edges,
-                                                       chunks_iter))
-                            if t[0] is not None)
-        with open(edges_output_file, 'w') as fo:
-            for record in edge_tuples:
-                json.dump(record, fo)
-                fo.write("\n")
+    global _process_edges_row
+    chunks_iter = su.read_jsonl_file_chunks(edges_file, chunk_size)
+    process_chunk_of_edges = _make_process_chunk_of_edges(babel_db)
+    mapped_iter = map(process_chunk_of_edges,
+                      chunks_iter)
+    tqdm_iter = tqdm.tqdm(mapped_iter, total=estim_num_chunks, desc="Processing")
+    chained_iter = it.chain.from_iterable(tqdm_iter)
+    filtered_iter = filter(lambda t: t[0] is not None, chained_iter)
+    edges_iter = map(operator.itemgetter(0), filtered_iter)
+    su.write_jsonl_file(edges_iter, edges_output_file)
 
 if __name__ == "__main__":
     main(**su.namespace_to_dict(_get_args()))
